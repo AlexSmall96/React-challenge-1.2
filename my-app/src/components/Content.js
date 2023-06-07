c2134125406868c4fde8fddc5749ba33791bebfd
AlexSmall96 <alex.small739@gmail.com>
2023-06-07 11:32:27 +0000

    "Component lifecycles challenge"

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css';
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem';
+import Loader from './Loader'
 
 export default class Content extends Component {
-    // constructor(props) {
-    //   super(props)
-    // }
-  render() {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isLoaded:false
+    }
+  }
+  
+  getData(){
+    setTimeout(() => {
+      this.setState({
+        isLoaded:true
+      })
+    }, 3000)
+  }
+  componentDidMount(){
+    this.getData()
+  }
+  render() {  
     return (
       <div>
         <div className={css.TitleBar}>My Photos</div>
         <div className={css.SearchResults}>
-            <PostItem posts={savedPosts} />
+          {
+            this.state.isLoaded ? (
+              <PostItem posts={savedPosts} />
+            ) : (
+              <Loader />
+            )
+          }
+            
         </div>
       </div>
     )
